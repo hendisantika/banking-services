abe7a37495571bfc5d8681e1c70bcc8fc1c6e4b2
hendisantika <hendisantika@yahoo.co.id>
2025-12-14 06:33:55 +0700

    feat: Add SecurityConfig🫸🌀✏️📗🐧🐳

---
+++ services/api-gateway/src/main/java/id/my/hendisantika/apigateway/SecurityConfig.java
@@ -0,0 +1,57 @@
+package id.my.hendisantika.apigateway;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.security.authentication.AbstractAuthenticationToken;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
+import org.springframework.security.oauth2.server.resource.authentication.ReactiveJwtAuthenticationConverterAdapter;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+import reactor.core.publisher.Mono;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : banking-services
+ * User: hendisantika
+ * Link: s.id/hendisantika
+ * Email: hendisantika@yahoo.co.id
+ * Telegram : @hendisantika34
+ * Date: 14/12/25
+ * Time: 06.33
+ * To change this template use File | Settings | File Templates.
+ */
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http) {
+        http.authorizeExchange(exchange -> exchange
+                .pathMatchers("/api/accounts/testRetry",
+                        "/api/accounts/testTimeout",
+                        "/api/accounts/testCircuitBreaker",
+                        "/api/accounts/testRateLimiter").permitAll()
+
+                .pathMatchers("/api/accounts/**",
+                        "/api/customer/**",
+                        "/api/transactions/**").hasRole("ACCOUNTS")
+
+                .pathMatchers("/api/cards/**").hasRole("CARDS")
+                .pathMatchers("/api/loans/**").hasRole("LOANS")
+        ).oauth2ResourceServer(spec ->
+                spec.jwt(
+                        jwtSpec ->
+                                jwtSpec.jwtAuthenticationConverter(jwtAuthenticationConverter()
+                                )
+                )
+        ).csrf(ServerHttpSecurity.CsrfSpec::disable);
+        return http.build();
+    }
+
+    private Converter<Jwt, Mono<AbstractAuthenticationToken>> jwtAuthenticationConverter() {
+        JwtAuthenticationConverter jac = new JwtAuthenticationConverter();
+        jac.setJwtGrantedAuthoritiesConverter(new KeycloakRoleConverter());
+        return new ReactiveJwtAuthenticationConverterAdapter(jac);
+    }
+}
