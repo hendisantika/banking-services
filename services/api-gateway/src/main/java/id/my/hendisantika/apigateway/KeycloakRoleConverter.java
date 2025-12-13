cb19a2af86ee82cf30fb3b7fdd1c0b5a83f1681b
hendisantika <hendisantika@yahoo.co.id>
2025-12-14 06:34:36 +0700

    feat: Add KeycloakRoleConverter🫸🌀✏️📗🐧🐳

---
+++ services/api-gateway/src/main/java/id/my/hendisantika/apigateway/KeycloakRoleConverter.java
@@ -0,0 +1,40 @@
+package id.my.hendisantika.apigateway;
+
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.oauth2.jwt.Jwt;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : banking-services
+ * User: hendisantika
+ * Link: s.id/hendisantika
+ * Email: hendisantika@yahoo.co.id
+ * Telegram : @hendisantika34
+ * Date: 14/12/25
+ * Time: 06.34
+ * To change this template use File | Settings | File Templates.
+ */
+public class KeycloakRoleConverter implements Converter<Jwt, Collection<GrantedAuthority>> {
+    @Override
+    public Collection<GrantedAuthority> convert(Jwt source) {
+        Map<String, Object> realmAccess = (Map<String, Object>) source.getClaims().get("realm_access");
+        if (realmAccess == null || realmAccess.isEmpty()) {
+            return new ArrayList<>();
+        }
+
+        Collection<GrantedAuthority> roles = ((List<String>) realmAccess.get("roles"))
+                .stream()
+                .map(role -> new SimpleGrantedAuthority("ROLE_" + role))
+                .collect(Collectors.toList());
+
+        return roles;
+    }
+}
