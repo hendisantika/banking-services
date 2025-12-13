23994df3759ead23253eac701ca9cd38bf39fff7
hendisantika <hendisantika@yahoo.co.id>
2025-12-13 09:26:27 +0700

    feat: Add AccountsConstants🫸🌀✏️📗🐧🐳

---
+++ services/account-service/src/main/java/id/my/hendisantika/accountservice/common/AccountsConstants.java
@@ -0,0 +1,24 @@
+package id.my.hendisantika.accountservice.common;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : banking-services
+ * User: hendisantika
+ * Link: s.id/hendisantika
+ * Email: hendisantika@yahoo.co.id
+ * Telegram : @hendisantika34
+ * Date: 13/12/25
+ * Time: 09.25
+ * To change this template use File | Settings | File Templates.
+ */
+public final class AccountsConstants {
+    public static final String SAVINGS = "Savings";
+    public static final String ADDRESS = "123 Main Street, New York";
+    public static final String STATUS_201 = "201";
+    public static final String MESSAGE_201 = "Account created successfully";
+    public static final String STATUS_200 = "200";
+    public static final String MESSAGE_200 = "Request processed successfully";
+    public static final String STATUS_417 = "417";
+    public static final String MESSAGE_417_UPDATE = "Update operation failed. Please try again or contact Dev team";
+    public static final String MESSAGE_417_DELETE = "Delete operation failed. Please try again or contact Dev team";
+}
