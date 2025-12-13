17d986fab1e77033c310c2a078390aa8039b4334
hendisantika <hendisantika@yahoo.co.id>
2025-12-13 09:33:31 +0700

    feat: Add Customer Entity🫸🌀✏️📗🐧🐳

---
+++ services/account-service/src/main/java/id/my/hendisantika/accountservice/entity/Customer.java
@@ -0,0 +1,44 @@
+package id.my.hendisantika.accountservice.entity;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : banking-services
+ * User: hendisantika
+ * Link: s.id/hendisantika
+ * Email: hendisantika@yahoo.co.id
+ * Telegram : @hendisantika34
+ * Date: 13/12/25
+ * Time: 09.33
+ * To change this template use File | Settings | File Templates.
+ */
+@Entity
+@Table(name = "customers")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Customer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @NotBlank
+    private String name;
+
+    @NotBlank
+    private String email;
+
+    private String phone;
+
+    @OneToOne(mappedBy = "customer")
+    private Account account;
+}
