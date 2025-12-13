d7543bb251942595f0e97af16e5cb42a9e7e2fdb
hendisantika <hendisantika@yahoo.co.id>
2025-12-14 06:15:55 +0700

    feat: Add @EnableConfigServer🫸🌀✏️📗🐧🐳

--- services/config-server/src/main/java/id/my/hendisantika/configserver/ConfigServerApplication.java
+++ services/config-server/src/main/java/id/my/hendisantika/configserver/ConfigServerApplication.java
@@ -2,8 +2,10 @@ package id.my.hendisantika.configserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
+@EnableConfigServer
 public class ConfigServerApplication {
 
     static void main(String[] args) {
