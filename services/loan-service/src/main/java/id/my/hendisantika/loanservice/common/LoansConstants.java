68fa3034262f29174ee92e7539bfbbd72b91192f
hendisantika <hendisantika@yahoo.co.id>
2025-12-14 06:44:43 +0700

    feat: Add LoansConstants🫸🌀✏️📗🐧🐳

---
+++ services/loan-service/src/main/java/id/my/hendisantika/loanservice/common/LoansConstants.java
@@ -0,0 +1,24 @@
+package id.my.hendisantika.loanservice.common;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : banking-services
+ * User: hendisantika
+ * Link: s.id/hendisantika
+ * Email: hendisantika@yahoo.co.id
+ * Telegram : @hendisantika34
+ * Date: 14/12/25
+ * Time: 06.44
+ * To change this template use File | Settings | File Templates.
+ */
+public final class LoansConstants {
+    public static final String HOME_LOAN = "Home Loan";
+    public static final Double NEW_LOAN_LIMIT = 1_00_000d;
+    public static final String STATUS_201 = "201";
+    public static final String MESSAGE_201 = "Loan created successfully";
+    public static final String STATUS_200 = "200";
+    public static final String MESSAGE_200 = "Request processed successfully";
+    public static final String STATUS_417 = "417";
+    public static final String MESSAGE_417_UPDATE = "Update operation failed. Please try again or contact Dev team";
+    public static final String MESSAGE_417_DELETE = "Delete operation failed. Please try again or contact Dev team";
+}
