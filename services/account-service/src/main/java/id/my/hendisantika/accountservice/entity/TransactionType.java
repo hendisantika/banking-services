186da7d6b043ba18c98bdf7e680b879d39425c77
hendisantika <hendisantika@yahoo.co.id>
2025-12-13 09:34:37 +0700

    feat: Add TransactionType Entity🫸🌀✏️📗🐧🐳

---
+++ services/account-service/src/main/java/id/my/hendisantika/accountservice/entity/TransactionType.java
@@ -0,0 +1,18 @@
+package id.my.hendisantika.accountservice.entity;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : banking-services
+ * User: hendisantika
+ * Link: s.id/hendisantika
+ * Email: hendisantika@yahoo.co.id
+ * Telegram : @hendisantika34
+ * Date: 13/12/25
+ * Time: 09.34
+ * To change this template use File | Settings | File Templates.
+ */
+public enum TransactionType {
+    WITHDRAWAL,
+    DEPOSIT,
+    TRANSFER
+}
