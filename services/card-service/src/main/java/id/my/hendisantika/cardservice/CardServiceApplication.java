eb519663c07a51a1dbffa13e9117606e445e8330
hendisantika <hendisantika@yahoo.co.id>
2025-12-13 18:05:45 +0700

    feat: Add card-service🫸🌀✏️📗🐧🐳

---
+++ services/card-service/src/main/java/id/my/hendisantika/cardservice/CardServiceApplication.java
@@ -0,0 +1,13 @@
+package id.my.hendisantika.cardservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CardServiceApplication {
+
+    static void main(String[] args) {
+        SpringApplication.run(CardServiceApplication.class, args);
+    }
+
+}
