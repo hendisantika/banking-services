49b0138ff86e13e96d4d30520e76467b0d1c0139
hendisantika <hendisantika@yahoo.co.id>
2025-12-13 09:29:54 +0700

    feat: Add CustomerDto🫸🌀✏️📗🐧🐳

---
+++ services/account-service/src/main/java/id/my/hendisantika/accountservice/dto/CustomerDto.java
@@ -0,0 +1,39 @@
+package id.my.hendisantika.accountservice.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+import lombok.*;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : banking-services
+ * User: hendisantika
+ * Link: s.id/hendisantika
+ * Email: hendisantika@yahoo.co.id
+ * Telegram : @hendisantika34
+ * Date: 13/12/25
+ * Time: 09.29
+ * To change this template use File | Settings | File Templates.
+ */
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class CustomerDto {
+
+    @NotEmpty(message = "Name can not be a null or empty")
+    @Size(min = 5, max = 30, message = "The length of the customer name should be between 5 and 30")
+    private String name;
+
+    @NotEmpty(message = "Email address can not be a null or empty")
+    @Email(message = "Email address should be a valid value")
+    private String email;
+
+    @Pattern(regexp = "(^$|[0-9]{10})", message = "Mobile number must be 10 digits")
+    private String phone;
+
+    private AccountDto accountDto;
+}
