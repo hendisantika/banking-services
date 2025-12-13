f830d4433cf425c47cd90a28627863a845b8a455
hendisantika <hendisantika@yahoo.co.id>
2025-12-14 06:36:55 +0700

    feat: Modify Main Class🫸🌀✏️📗🐧🐳

--- services/api-gateway/src/main/java/id/my/hendisantika/apigateway/ApiGatewayApplication.java
+++ services/api-gateway/src/main/java/id/my/hendisantika/apigateway/ApiGatewayApplication.java
@@ -2,9 +2,27 @@ package id.my.hendisantika.apigateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.filter.ratelimit.KeyResolver;
+import org.springframework.cloud.gateway.filter.ratelimit.RedisRateLimiter;
+import org.springframework.context.annotation.Bean;
+import reactor.core.publisher.Mono;
 
 @SpringBootApplication
 public class ApiGatewayApplication {
+    @Bean
+    public RedisRateLimiter redisRateLimiter() {
+        return new RedisRateLimiter(1, 2, 2);
+    }
+
+    @Bean
+    public KeyResolver keyResolver() {
+        return exchange -> Mono.just(exchange
+                .getRequest()
+                .getRemoteAddress()
+                .getAddress()
+                .getHostAddress()
+        );
+    }
 
     static void main(String[] args) {
         SpringApplication.run(ApiGatewayApplication.class, args);
