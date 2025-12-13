73928eee3771a25b6ea90da542c5bbf6eb335e5c
hendisantika <hendisantika@yahoo.co.id>
2025-12-14 06:49:20 +0700

    feat: Add ResourceNotFoundException🫸🌀✏️📗🐧🐳

---
+++ services/loan-service/src/main/java/id/my/hendisantika/loanservice/exception/ResourceNotFoundException.java
@@ -0,0 +1,18 @@
+package id.my.hendisantika.loanservice.exception;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : banking-services
+ * User: hendisantika
+ * Link: s.id/hendisantika
+ * Email: hendisantika@yahoo.co.id
+ * Telegram : @hendisantika34
+ * Date: 14/12/25
+ * Time: 06.49
+ * To change this template use File | Settings | File Templates.
+ */
+public class ResourceNotFoundException extends RuntimeException {
+    public ResourceNotFoundException(String resourceName, String fieldName, String fieldValue) {
+        super(String.format("%s not found with the given input data %s : '%s'", resourceName, fieldName, fieldValue));
+    }
+}
