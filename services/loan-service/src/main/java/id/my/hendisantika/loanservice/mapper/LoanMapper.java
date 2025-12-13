c21b46c9250612a3b877a565c131c9eb4891f922
hendisantika <hendisantika@yahoo.co.id>
2025-12-14 06:51:16 +0700

    feat: Add LoanMapper🫸🌀✏️📗🐧🐳

---
+++ services/loan-service/src/main/java/id/my/hendisantika/loanservice/mapper/LoanMapper.java
@@ -0,0 +1,39 @@
+package id.my.hendisantika.loanservice.mapper;
+
+import id.my.hendisantika.loanservice.dto.LoanDto;
+import id.my.hendisantika.loanservice.entity.Loan;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : banking-services
+ * User: hendisantika
+ * Link: s.id/hendisantika
+ * Email: hendisantika@yahoo.co.id
+ * Telegram : @hendisantika34
+ * Date: 14/12/25
+ * Time: 06.50
+ * To change this template use File | Settings | File Templates.
+ */
+public class LoanMapper {
+    public static LoanDto toDto(Loan loans) {
+        LoanDto loanDto = new LoanDto();
+        loanDto.setLoanNumber(loans.getLoanNumber());
+        loanDto.setLoanType(loans.getLoanType());
+        loanDto.setMobileNumber(loans.getMobileNumber());
+        loanDto.setTotalLoan(loans.getTotalLoan());
+        loanDto.setAmountPaid(loans.getAmountPaid());
+        loanDto.setOutstandingAmount(loans.getOutstandingAmount());
+        return loanDto;
+    }
+
+    public static Loan toEntity(LoanDto loanDto) {
+        Loan loan = new Loan();
+        loan.setLoanNumber(loanDto.getLoanNumber());
+        loan.setLoanType(loanDto.getLoanType());
+        loan.setMobileNumber(loanDto.getMobileNumber());
+        loan.setTotalLoan(loanDto.getTotalLoan());
+        loan.setAmountPaid(loanDto.getAmountPaid());
+        loan.setOutstandingAmount(loanDto.getOutstandingAmount());
+        return loan;
+    }
+}
